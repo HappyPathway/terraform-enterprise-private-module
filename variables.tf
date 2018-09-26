bf28c96790a3d4935ca2189980aa92bc968d8d45
David Arnold <dja+djaboxx@sonic.net>
2018-09-26 11:23:09 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,16 @@ variable "oauth_token" {
   type = "string"
 }
 
-variable "tfe_org" {}
+variable "tfe_org" {
+  type = "string"
+  description = "Name of TFE Organization"
+}
+
+variable "tfe_token" {
+  type = "string"
+  description = "TFE User Token"
+}
+
 
 variable "config" {
   default = "~/.terraformrc"
